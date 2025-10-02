5c8144c8896114a2e4a7defdaa460c2ed4c98782
Matt Casanova <lazersquad@gmail.com>
2025-10-01 22:07:13 -0700

    30 Days to learn larvel ep 2

---
+++ resources/views/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+    content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <h1 class="text-4xl font-bold text-gray-800 dark:text-gray-200">Contact Page</h1>
+    </div>
+</body>
+</html>
