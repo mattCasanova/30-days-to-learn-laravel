ee6cf8c6160b518d0eb3c1ae573dcaf130740aea
Matt Casanova <lazersquad@gmail.com>
2025-10-02 08:08:14 -0700

    30 Days to learn larvel ep 5 no homework

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,6 @@
-<li><a {{ $attributes }}>{{ $slot }}</a></li>
+        @props(['active' => false])
+        <a {{ $attributes }} class="{{ $active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} block rounded-md px-3 py-2 text-base font-medium"
+            aria-current="{{ $active ? 'page' : false }}"
+            >
+            {{ $slot }}
+        </a>
