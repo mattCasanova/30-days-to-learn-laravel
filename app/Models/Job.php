c7fd79e312f8068769288990450d6b03e9357950
Matt Casanova <lazersquad@gmail.com>
2025-10-02 17:59:37 -0700

    30 Days to learn laravel ep 09

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,36 +2,11 @@
 
 namespace App\Models;
 
-class Job
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
 {
-    public int $id;
-    public string $title;
-    public string $salary;
+    protected $table = 'job_listings';
 
-    public function __construct(int $id, string $title, string $salary)
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->salary = $salary;
-    }
-
-    public static function all(): array
-    {
-        return [
-            new Job(1, 'Director of Product', '$50,000'),
-            new Job(2, 'Senior Developer', '$40,000'),
-            new Job(3, 'Junior Developer', '$30,000'),
-        ];
-    }
-
-    public static function find(int $id): ?Job
-    {
-        $job = collect(self::all())->firstWhere('id', $id);
-
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
