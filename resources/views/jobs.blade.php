675b199e2293cbab1a1f917c13274f721b9499fa
Matt Casanova <lazersquad@gmail.com>
2025-10-02 20:59:47 -0700

    30 Days to learn laravel ep 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,14 +1,17 @@
 <x-layout>
     <x-slot:heading>Jobs Listings</x-slot:heading>
 
-    <ul class="space-y-2">
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li class="text-white">
-                <a href="/jobs/{{ $job->id }}" class="text-blue-300 hover:underline">
+            <a href="/jobs/{{ $job->id }}" class="block px-4 py-6 border border-gray-700 rounded-lg max-w-3xl">
+                <div class="font-bold text-sm text-blue-200 mb-2 hover:underline">
+                    Posted by {{ $job->employer->name }}
+                </div>
+                <div class="text-blue-300">
                     <strong>{{ $job->title }}</strong>: Pays {{ $job->salary }} per year.
-                </a>
-            </li>
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
