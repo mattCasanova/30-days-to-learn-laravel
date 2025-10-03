9db3872d9b349cf63b2bc57a5114f3955034a5dc
Matt Casanova <lazersquad@gmail.com>
2025-10-02 22:17:03 -0700

    30 Days to learn laravel ep 16

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,37 @@ Route::get('/jobs', function () {
     //$jobs = Job::with('employer', 'tags')->cursorPaginate(8);
 
     // Best for simple pagination without total count and good url generation
-    $jobs = Job::with('employer', 'tags')->simplePaginate(8);
-    return view('jobs', [
+    $jobs = Job::with('employer', 'tags')->latest()->simplePaginate(8);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    return view('job', ['job' => Job::find($id)]);
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
+// uses wildcard {id} to capture the job id from the URL
+// e.g. /jobs/5 would capture 5 as the id
+// This could be conflicted with other routes like /jobs/create
+// so the order of route definitions matters
+// More specific routes should be defined before more general ones
+Route::get('/jobs/{id}', function ($id) {
+    return view('jobs.show', ['job' => Job::find($id)]);
+});
+
+Route::post('/jobs', function () {
+    $attributes = request()->validate([
+        'title' => 'required|max:255',
+        'salary' => 'required|max:255',
+    ]);
+
+    // For now, hardcode employer_id to 1
+    $attributes = array_merge($attributes, ['employer_id' => 1]);
+    $job = Job::create($attributes);
+
+    return redirect('/jobs');
+});
 
 Route::get('/contact', function () {
     return view('contact');
